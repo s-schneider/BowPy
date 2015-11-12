6822e831127c82b9912a6df06a81838973e21175
Simon Schneider <s_schn42@PGP157.NWZ.WWU.DE>
2015-11-12 15:31:50 +0100

    reading data and creating array

--- fk_work.py
+++ fk_work.py
@@ -11,6 +11,9 @@ import obspy
 from obspy import read
 import numpy as np
 import matplotlib as plt
+import Muenster_Array_Seismology as MAS
+from Muenster_Array_Seismology import get_coords
+from obspy.core.util.geodetics import gps2DistAzumiuth
 
 
 def fk_filter(stream, inventory, catalog, phase):
@@ -39,20 +42,33 @@ def fk_filter(stream, inventory, catalog, phase):
 	inv = obspy.read_inventory(inventory)
 	cat = obspy.readEvents(catalog)
 
-	tr0 = st[0]
-	tr1 = st[1]
-
-	x = tr0.data
-	y = tr1.data
+	#pushing the trace data in an array
+	ArrayData = np.array([st[0].data])
+	
+	for i in range(len(st))[1:]:
+	  next_st = np.array([st[i].data])
+	  ArrayData = np.append(ArrayData, next_st , axis=0)
+	
+	
 
 
 	"""
 	Binning of the data ############################################################
 	"""
-	#pushing the trace data in an array
-
+	
 	#calc min and max epidist between source and receivers
-
+	Array_Coords = get_coords(inv)
+	
+	stat1 = st[i].meta.station
+	stat2 = st[i+1].meta.station
+	lat1 = Array_Coords[""][""]
+	lon1 = Array_Coords[""][""]
+	
+	lat2 = Array_Coords[""][""]
+	lon2 = Array_Coords[""][""]
+	
+	epi_dist = gps2DistAzumiuth( lat1, lon1, lat2, lon2 )
+	
 	#create equidistant (delta x) x-mesh with ->  N artificial receiver / ghost receiver
 
 	#assign stations to ghost receiver
@@ -75,32 +91,32 @@ def fk_filter(stream, inventory, catalog, phase):
 	"""
 	#apply 2D FFT
 
-	np.fft.fft2(a, s=None, axes=(-2, -1))
-		"""
-		Parameters
-		----------
-		a : array_like
-		    Input array, can be complex
-		s : sequence of ints, optional
-		    Shape (length of each transformed axis) of the output
-		    (`s[0]` refers to axis 0, `s[1]` to axis 1, etc.).
-		    This corresponds to `n` for `fft(x, n)`.
-		    Along each axis, if the given shape is smaller than that of the input,
-		    the input is cropped.  If it is larger, the input is padded with zeros.
-		    if `s` is not given, the shape of the input along the axes specified
-		    by `axes` is used.
-		axes : sequence of ints, optional
-		    Axes over which to compute the FFT.  If not given, the last two
-		    axes are used.  A repeated index in `axes` means the transform over
-		    that axis is performed multiple times.  A one-element sequence means
-		    that a one-dimensional FFT is performed.
+	#np.fft.fft2(a, s=None, axes=(-2, -1))
+	"""
+	Parameters
+	----------
+	a : array_like
+	Input array, can be complex
+	s : sequence of ints, optional
+	Shape (length of each transformed axis) of the output
+	(`s[0]` refers to axis 0, `s[1]` to axis 1, etc.).
+	This corresponds to `n` for `fft(x, n)`.
+	Along each axis, if the given shape is smaller than that of the input,
+	the input is cropped.  If it is larger, the input is padded with zeros.
+	if `s` is not given, the shape of the input along the axes specified
+	by `axes` is used.
+	axes : sequence of ints, optional
+	Axes over which to compute the FFT.  If not given, the last two
+	axes are used.  A repeated index in `axes` means the transform over
+	that axis is performed multiple times.  A one-element sequence means
+	that a one-dimensional FFT is performed.
 
-		Returns
-		-------
-		out : complex ndarray
-		    The truncated or zero-padded input, transformed along the axes
-		    indicated by `axes`, or the last two axes if `axes` is not given.
-		"""
+	Returns
+	-------
+	out : complex ndarray
+	The truncated or zero-padded input, transformed along the axes
+	indicated by `axes`, or the last two axes if `axes` is not given.
+	"""
 
 	#mute area around |f| > eps, choose eps dependent on your phase/data/i dont know yet
 
@@ -114,10 +130,10 @@ def fk_filter(stream, inventory, catalog, phase):
 	return stream with filtered and binned data ####################################
 	"""
 
+stream="2011-03-11T05:46:23.MSEED"
+inventory="2011-03-11T05:46:23.MSEED_inv.xml"
+catalog="2011-03-11T05:46:23.MSEED_cat.xml"
+phase="PP"
 
-
-
-
-
-
+fk_filter(stream, inventory, catalog, phase)
 
