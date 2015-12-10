86d48108babb53e707b71c9c51ab93c6dce7fc5e
Simon Schneider <s_schn42@PGP157.NWZ.WWU.DE>
2015-12-10 11:34:19 +0100

    new structure

---
+++ dev/do_fkextract.py
@@ -0,0 +1,18 @@
+import obspy
+import numpy as np
+from numpy import genfromtxt
+import fk_work as fkw
+from fk_work import fk_filter_extract_phase
+
+
+st=obspy.read("FKOUT.QHD")
+
+#epi = genfromtxt("epidist.txt", dtype=None, usecols=1)
+
+data = fkw.stream2array(st)
+
+data_fil = fk_filter_extract_phase(data)
+
+st_fil = fkw.array2stream(data_fil)
+
+st_fil.write("FKIN.QHD", format="Q")
