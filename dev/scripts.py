7a4d47fddd9e257fc31084d2c73dae0a2e0748b5
s-schneider <simon.schneider@wwu.de>
2016-02-17 15:58:24 -0700

    new figures

--- dev/scripts.py
+++ dev/scripts.py
@@ -139,7 +139,7 @@ import fk_work as fkw
 from fk_work import fk_filter
 import instaseis as ins
 
-uniform=True
+uniform=False
 db = ins.open_db("/Users/Simon/dev/instaseis/10s_PREM_ANI_FORCES")
 
 tofe = obspy.UTCDateTime(2016,2,9,18,41,1)
@@ -172,7 +172,7 @@ with open("SYNTH05.QST", "w") as fh:
 		randrange = np.random.rand(20)*10. +100.
 		randrange[0] = 100.
 		randrange[19] = 19. * stationstep + 100.
-		while randrange.max() > randrage[19]:
+		while randrange.max() > randrange[19]:
 			i = randrange.argmax()
 			randrange[i] = randrange[i]-0.1
 		randrange.sort()
