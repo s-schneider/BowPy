5c9923a738ecd0179e75f1f440a9803dbde1e5e1
s-schneider <simon.schneider@wwu.de>
2016-02-19 21:58:11 -0700

    Documentation and preparation for SSA

--- dev/fkutilities.py
+++ dev/fkutilities.py
@@ -9,249 +9,21 @@ import datetime
 import scipy as sp
 import scipy.signal as signal
 
-def fk_filter(st, ftype=None, inv=None, cat=None, phase=None, epi_dist=None, fktype=None, normalize=False):
-	"""
-	At this point prework with programs like seismic handler or SAC is needed to perform correctly
+"""
+A collection of useful functions for handling the fk_filter and seismic data.
 
+Author: S. Schneider 2015
 
-	Import stream, the function applies an 2D FFT, removes a certain window around the
-	desired phase to surpress a slownessvalue corresponding to a wavenumber and applies an 2d iFFT.
-	Alternative is an nonequidistant 2D Lombard-Scargle transformation.
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+any later version.
 
-	param st: Stream
-	type st: obspy.core.stream.Stream
-	
-	param ftype: Type of filter, FFT or LS (Lombard-Scargle)
-	type ftype: string
-
-	param inv: inventory
-	type inv: obspy.station.inventory.Inventory
-
-	param cat: catalog
-	type cat: obspy.core.event.Catalog
-
-	param phase: name of the phase to be investigated
-	type phase: string
-	
-	param epidist: list of epidistances, corresponding to st
-	type epidist: list
-
-	param fktype: type of fk-filter, extraction or elimination
-	type fktype: string
-	
-	param min_wavelength: minimum wavelength for wavenumber in degrees
-	type min_wavelength: int
-	
-	param max_wavelength: maximum wavelength for wavenumber in degrees
-	type max_wavelength: int
-	
-	param knout: number of k-values to be checked
-	type knout: int
-	"""
-
-
-
-	"""
-	2D Wavenumber-Frequency Filter #########################################################
-	"""
-	# 2D FFT-LS #####################################################################
-	if ftype == "LS":
-		print("using LS")
-		tstart=datetime.datetime.now()
-		ArrayData = stream2array(st)
-		if st and inv and cat:
-			epidist = epidist2nparray(epidist_stream(st, inv, cat))
-		
-		if not epi_dist == None:
-			epidist = epi_dist
-
-		if fktype == "eliminate":
-			array_filtered, periods = _fk_ls_filter_eliminate_phase_sp(ArrayData, y_dist=epidist)
-		elif fktype == "extract":
-			array_filtered, periods = _fk_ls_filter_extract_phase_sp(ArrayData, y_dist=epidist)
-		else:
-			print("No type of fk-filter specified")
-			raise TypeError		
-			
-		fkspectra=array_filtered
-		tend=datetime.datetime.now()
-		print( tend-tstart )
-		return(fkspectra, periods)
-
-		"""
-		perform ifft using fkspectra and periods, should be possible!
-		"""
-	#2D FFT #########################################################################
-	
-	elif ftype == "FFT":
-		print("using FFT")
-		tstart=datetime.datetime.now()
-		#Convert to numpy.ndarray, stream info still in st
-		ArrayData = stream2array(st, normalize)
-		
-		#Apply FFT
-		if fktype == "eliminate":
-			array_filtered = _fk_fft_filter_eliminate_phase(ArrayData, radius=None)
-		elif fktype == "extract":
-			array_filtered = _fk_fft_filter_extract_phase(ArrayData, radius=None)
-		else:
-			print("No type of fk-filter specified")
-			raise TypeErr
-		#Convert to Stream object
-		stream_filtered = array2stream(array_filtered)
-
-		for i in range(len(stream_filtered)):
-			stream_filtered[i].meta = st[i].meta
-
-		tend=datetime.datetime.now()
-		print(tend-tstart)
-		return(stream_filtered)
-
-	else:
-		print("No valid input for type of filter")
-		raise TypeError
-
-	"""
-	return stream with filtered data ####################################
-	"""
-	
-# LS FUNCTIONS ############################################################################
-def _fk_ls_filter_extract_phase_sp(ArrayData, y_dist=False, radius=None, maxk=False):
-	"""
-	Only use with the function fk_filter!
-	FK-filter using the Lomb-Scargle Periodogram with the scipy library
-	param data:	data of the array
-	type data:	numpy.ndarray
-
-	param snes:	slownessvalue of the desired extracted phase
-	type snes:	int
-	"""
-	return()
-
-def _fk_ls_filter_eliminate_phase_sp(ArrayData, y_dist=False, radius=None, maxk=False):
-	"""
-	Only use with the function fk_filter!
-	Function to test the fk workflow with synthetic data
-	param data:	data of the array
-	type data:	numpy.ndarray
-
-	param snes:	slownessvalue of the desired extracted phase
-	type snes:	int
-	"""
-	epidist=y_dist
-	freq = np.zeros((len(ArrayData), len(ArrayData[0]) / 2  + 1)) + 1j
-
-	for i in range(len(ArrayData)):
-		freq_new = np.fft.rfftn(ArrayData[i])
-		freq[i] = freq_new
-
-	freqT = transpose(freq)
-	knum = np.zeros( ( len(freqT), len(freqT[0])  /2 +1 ))
-		     
-	#calc best range
-	N = len(freqT[0])
-	#max_bound = ( max_wavelength/(min_wavelength*N**2) ) *N**2 * min_wavelength
-	dN = ( max(freqT[0]) - min(freqT[0]) / N )
-	
-	f_temp = np.fft.rfftfreq(len(freqT[0]), dN) * 2.* np.pi
-
-	#1. try: 
-	#period_range = np.linspace(min_wavelength, max_bound, len(freqT[0]))
-	#2. try: 
-	#period_range = np.linspace(f_temp[1], max(f_temp), N)
-	#3. try:
-	period_range = f_temp
-	#period_range = period_range.astype('float')
-	period_range[0] = 1.
-	#after this change the first outputparameter of the periodogram to a 
-	#correlation between the signal and a e^0 function ;)
-	period_range = period_range.astype('float')
-	
-	for j in range(len(freqT)):
-		k_new = signal.lombscargle(epidist, abs(freqT[j]), period_range)
-		k_new = ls2ifft_prep(k_new, abs(freqT[j]))
-		knum[j] = k_new
-
-			
-	#change dtype to integer, for further processing
-	period_range = period_range.astype('int')
-	fkspectra = knum
-	if maxk:
-		max_k = maxrow(fkspectra)
-		print("maximum wavenumber k is %f" % max_k)
-		dsfft = line_set_zero(fkspectra, max_k)
-	else:
-		dsfft = line_set_zero(fkspectra, 0, radius)
-	
-	return(transpose(fkspectra), period_range)
-
-
-
-# FFT FUNCTIONS ############################################################################
-
-def _fk_fft_filter_extract_phase(data, snes=False, y_dist=False, radius=None, maxk=False):
-	"""
-	Only use with the function fk_filter!
-	Function to test the fk workflow with synthetic data
-	param data:	data of the array
-	type data:	numpy.ndarray
-
-	param snes:	slownessvalue of the desired extracted phase
-	type snes:	int
-	"""
-	if snes:
-		ds = shift_array(data, snes, y_dist)
-	else:
-		ds = data
-	
-	dsfft = np.fft.fftn(ds)
-	
-	if maxk:
-		max_k = maxrow(dsfft)
-		print("maximum wavenumber k is %f" % max_k)
-		dsfft = line_cut(dsfft, max_k)
-	else:
-		dsfft = line_cut(dsfft, 0,radius)
-	
-	ds = np.fft.ifftn(dsfft)
-	if snes:
-		data_fk = shift_array(ds, -snes, y_dist)
-	else:
-		data_fk = ds
-
-	return(data_fk.real)
-
-def _fk_fft_filter_eliminate_phase(data, snes=False, y_dist=False, radius=None, maxk=False):
-	"""
-	Only use with the function fk_filter!
-	Function to test the fk workflow with synthetic data
-	param data:	data of the array
-	type data:	numpy.ndarray
-
-	param snes:	slownessvalue of the desired extracted phase
-	type snes:	int
-	"""
-	if snes:
-		ds = shift_array(data, snes, y_dist)
-	else:
-		ds = data
-		
-	dsfft = np.fft.fftn(ds)
-	
-	if maxk:
-		max_k = maxrow(dsfft)
-		print("maximum wavenumber k is %f" % max_k)
-		dsfft = line_set_zero(dsfft, max_k)
-	else:
-		dsfft = line_set_zero(dsfft, 0, radius)
-	
-	ds = np.fft.ifftn(dsfft)
-	if snes:
-		data_fk = shift_array(ds, -snes, y_dist)
-	else:
-		data_fk = ds
-
-	return(data_fk.real)
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details: http://www.gnu.org/licenses/
+"""
 
 def create_sine( no_of_traces=10, len_of_traces=30000, samplingrate = 30000,
                  no_of_periods=1):
@@ -653,8 +425,9 @@ def kill(data, stat):
 	data = np.delete(data, stat, 0)
 	return(data)
 
-def line_cut(array, stat, radius=None):
+def line_cut_old(array, stat, radius=None):
 	"""
+	Old Version, if the new works fine --> delete
 	Sets the array to zero, except for the stat line and the radius values around it.
 	"Cuts" one line out. 
 	"""
@@ -674,6 +447,26 @@ def line_cut(array, stat, radius=None):
 			else:
 				new_array[y-i] = array[y-i]
 	
+	else:
+		new_array[stat] = array[stat]
+
+	
+	return(new_array)
+
+def line_cut(array, stat, radius=None):
+	"""
+	Sets the array to zero, except for the stat line and the radius values around it.
+	"Cuts" one line out. 
+	"""
+	new_array = np.zeros( (len(array[0]),len(array)) )
+
+	if radius:
+		for i in range(stat, stat + radius  + 1):
+			new_array[i] = array[i]
+			if i == 0:
+				new_array[y] = array[y]
+			else:
+				new_array[y-i] = array[y-i]
 	else:
 		new_array[stat] = array[stat]
 
