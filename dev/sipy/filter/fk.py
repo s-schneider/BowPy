efd196f2071541d99b192890745b482f2a77c168
Simon Schneider <simon.schneider@wwu.de>
2016-07-04 09:43:08 +0200

    Update fk.py

--- dev/sipy/filter/fk.py
+++ dev/sipy/filter/fk.py
@@ -23,7 +23,7 @@ from sipy.util.fkutil import ls2ifft_prep, line_cut, line_set_zero, shift_array,
 from sipy.util.base import nextpow2, array2stream, array2trace, stream2array
 from sipy.util.picker import get_polygon
 
-def fk_filter(st, inv=None, event=None, ftype='extract', fshape=['spike', None, None], phase=None, polygon=12, normalize=True, stack=False,
+def fk_filter(st, inv=None, event=None, ftype='extract', fshape=['butterworth', 4, 4], phase=None, polygon=12, normalize=True, stack=False,
 					slopes=[-3,3], deltaslope=0.05, slopepicking=False, smoothpicks=False, dist=0.5, maskshape=['boxcar',None], 
 					order=4., peakinput=False, eval_mean=1):
 	"""
